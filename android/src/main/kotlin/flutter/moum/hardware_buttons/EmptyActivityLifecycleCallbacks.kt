e2a05e5b091c1a61bebe302348bdd08c35a725a6
lohith8099 <76481288+lohith8099@users.noreply.github.com>
2021-02-03 23:50:09 +0530

    Update EmptyActivityLifecycleCallbacks.kt

--- android/src/main/kotlin/flutter/moum/hardware_buttons/EmptyActivityLifecycleCallbacks.kt
+++ android/src/main/kotlin/flutter/moum/hardware_buttons/EmptyActivityLifecycleCallbacks.kt
@@ -10,13 +10,13 @@ abstract class EmptyActivityLifecycleCallbacks: Application.ActivityLifecycleCal
 
     override fun onActivityResumed(activity: Activity) { }
 
-    override fun onActivityStarted(activity: Activity?) { }
+    override fun onActivityStarted(activity: Activity) { }
 
-    override fun onActivityDestroyed(activity: Activity?) { }
+    override fun onActivityDestroyed(activity: Activity) { }
 
-    override fun onActivitySaveInstanceState(activity: Activity?, outState: Bundle?) { }
+    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) { }
 
-    override fun onActivityStopped(activity: Activity?) { }
+    override fun onActivityStopped(activity: Activity) { }
 
-    override fun onActivityCreated(activity: Activity?, savedInstanceState: Bundle?) { }
+    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle) { }
 }
